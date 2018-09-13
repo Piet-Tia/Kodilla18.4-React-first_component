2f488438eaef2756a5ff4590a70f211c73d9a013
Piotr Nastaj <piotr@nastaj.net>
2018-09-13 16:15:57 +0200

    Finished example exercise

---
+++ script.js
@@ -0,0 +1,23 @@
+const image = {
+	name: 'Kotek',
+	src: 'http://imgur.com/n8OYCzR.png'
+};
+
+const GalleryItem = React.createClass({
+	propTypes: {
+		image: React.PropTypes.object.isRequired,
+	},
+	render: function () {
+		return (
+			React.createElement('div', {},
+				React.createElement('h2', {}, this.props.image.name),
+				React.createElement('img', { src: this.props.image.src })
+			)
+		)
+	},
+});
+
+const element = React.createElement(GalleryItem);
+
+ReactDOM.render(element, document.getElementById('app'));
+
